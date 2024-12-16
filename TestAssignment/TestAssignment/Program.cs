51c2a02880259d5faee46f24a14eb81a679dba30
LornBunchuy <lorbunchuy@gmail.com>
2024-12-16 16:23:37 +0700

    For Code assignment
    Thank you for attention.

---
+++ TestAssignment/TestAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string[] Chanell = { "BE", "FE", "QA", "Urgent" };
+
+        Console.Write("Enter the notification title: ");
+        string notification = Console.ReadLine();
+
+        List<string> result = new List<string>();
+
+        foreach (string channel in Chanell)
+        {
+            int index = notification.IndexOf($"[{channel}]");
+            if (index != -1)
+            {
+                result.Add(channel); 
+            }
+        }
+        Console.WriteLine("Receive channels: " + string.Join(", ", result));
+    }
+}
